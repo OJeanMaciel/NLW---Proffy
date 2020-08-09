f1ec1fc508b663d1c09234831740454404ecaa3d
jean maciel <Jeansud@hotmail.com>
2020-08-09 02:13:52 -0300

    aula 4

--- src/server.js
+++ src/server.js
@@ -1,15 +1,101 @@
+const proffys = [
+    { name: "Diego Fernandes", 
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    whastapp: "89898989",
+    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Química",
+    cost: "20", 
+    weekday: [0],
+    time_from: [720],
+    time_to: [1220]
+    },
+    { name: "Daniele Evangelista", 
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    whastapp: "89898989",
+    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Química",
+    cost: "20", 
+    weekday: [1],
+    time_from: [720],
+    time_to: [1220]
+    },
+    
+    { name: "Jean Kevin Maciel", 
+    avatar: "https://avatars0.githubusercontent.com/u/68939572?s=460&u=08ca257898b4377c19fe853f6f414f80dfdb4b65&v=4",
+    whastapp: "89898989",
+    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Química",
+    cost: "20", 
+    weekday: [1],
+    time_from: [720],
+    time_to: [1220]
+    }
+]
+
+const subjects =[
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays =[
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+function getSubject(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
+}
+
+function pageGiveClasses(req, res) {
+    const data = req.query
+    const isNotEmpty = Object.keys(data).length > 0 
+    if (isNotEmpty) {
+        data.subject = getSubject(data.subject)
+
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+
+    return res.render("give-classes.html", { subjects, weekdays })
+}
+
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
-.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
-.get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
-})
-.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
-})
+
+server
+.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 
 .listen(5500)
